411e3e67d9ed813c7a1197bd2ad51764cdbfc7f8
Emil Schultz <emilrschultz@gmail.com>
2021-04-30 22:25:23 +0200

    Firebase setup

---
+++ config/firebase.js
@@ -0,0 +1,21 @@
+import firebase from "firebase";
+import "firebase/database";
+
+if (typeof window !== "undefined" && !firebase.apps.length) {
+  try {
+    firebase.initializeApp({
+      apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+      databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASEURL,
+    });
+    // firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export default firebase;
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,23 @@
+import firebase from "../config/firebase";
+import { useEffect, useState } from "react";
+
 export default function Home() {
-  return <p>Butterfingers</p>;
+  const [items, setItems] = useState();
+
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection("items")
+      .onSnapshot((querySnapshot) => {
+        setItems(querySnapshot.docs.map((item) => item.data()));
+      });
+  }, []);
+
+  console.log("ITEMS FROM FIRESTORE:", items);
+
+  return (
+    <>
+      <p>Butterfingers</p>
+    </>
+  );
 }
