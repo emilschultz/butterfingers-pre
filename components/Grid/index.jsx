4c1f2cad79e2763ba843ac27cf2c973387ea9d3d
Emil Schultz <emilrschultz@gmail.com>
2021-05-04 10:12:00 +0200

    Style:
    Navigation bar styling

---
+++ components/Grid/index.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 160px 160px 160px 160px 160px 160px 160px;
+  grid-template-rows: 160px 160px 160px 160px 160px;
+`;
+
+export default Grid;
