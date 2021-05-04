66b05dae9c17718765daf0777c95377c3209a6a6
Emil Schultz <emilrschultz@gmail.com>
2021-05-04 10:13:40 +0200

    feat: Light and dark theme mode set up

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,24 @@
+import { useState } from "react";
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme, GlobalStyles } from "../themeConfig";
+import NavBar from "../components/NavBar";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
+
+  return (
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <GlobalStyles />
+      <NavBar />
+      <button onClick={toggleTheme}>Switch Theme</button>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
+console.log({ lightTheme });
 
 export default MyApp;
