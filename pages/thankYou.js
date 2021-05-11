60f5b26315adc1b4c36bec3da4599cc0d3818fe8
Emil Schultz <emilrschultz@gmail.com>
2021-05-11 13:49:59 +0200

    feat: Clear data from state on page load

--- pages/thankYou.js
+++ pages/thankYou.js
@@ -1,4 +1,16 @@
+import { useStateMachine } from "little-state-machine";
+import clearAction from "../stateMachineActions/clearAction";
+import { useEffect } from "react";
+
 export default function ThankYou() {
+  const { state, actions } = useStateMachine({ clearAction });
+
+  useEffect(() => {
+    actions.clearAction();
+  }, []);
+
+  console.log(state);
+
   return (
     <>
       <p>Thank you!</p>
