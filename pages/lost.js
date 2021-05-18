e349811e263403d42344107ac045a35edaf09fea
Emil Schultz <emilrschultz@gmail.com>
2021-05-18 21:24:30 +0200

    feat: Map out search with data from firebase

--- pages/lost.js
+++ pages/lost.js
@@ -1,13 +1,11 @@
-import PageTitle from "../components/PageTitle";
 import firebase from "../config/firebase";
 import React, { useState } from "react";
-import { useStateMachine } from "little-state-machine";
 import { useCurrentItem } from "../context/EachItemContext";
 import { useRouter } from "next/router";
 
-import updateResultAction from "../stateMachineActions/updateResultAction";
-
 import Grid from "../components/Grid";
+import PageTitle from "../components/PageTitle";
+import HeadingOne from "../components/HeadingOne";
 
 export default function Lost() {
   const currentItem = useCurrentItem();
@@ -16,11 +14,6 @@ export default function Lost() {
   const [foundItems, setFoundItems] = useState([]);
   const [searchItem, setSearchItem] = useState("");
 
-  // little state machine
-  const { state, actions } = useStateMachine({ updateResultAction });
-  const currentState = state;
-
-  console.log(currentState);
   // get
   const firebaseQuery = (e) => {
     e.preventDefault();
@@ -42,59 +35,37 @@ export default function Lost() {
   const inputHandler = (event) => {
     event.preventDefault();
     let inputValue = event.target.value;
-    // let capitalizedFirstLetter =
-    //   inputValue.charAt(0).toUpperCase() + inputValue.slice(1);
-    setSearchItem(inputValue);
+    let capitalizedFirstLetter =
+      inputValue.charAt(0).toUpperCase() + inputValue.slice(1);
+    setSearchItem(capitalizedFirstLetter);
   };
 
-  console.log("FOUND ITEMS:", foundItems);
-
+  // map out items from firebase
   const foundItemslist = foundItems.map((item) => {
     return (
-      <div key={Math.random() * (100 - 1)}>
-        <p>What: {item.name}</p>
-        <p>Description: {item.description}</p>
-        <p>Now at: {item.currentlocation}</p>
-        <img style={{ width: "250px" }} src={item.image}></img>
+      <div
+        key={Math.random() * (100 - 1)}
+        onClick={() => {
+          currentItem.addItem({
+            name: item.name,
+            description: item.description,
+            droplocation: item.droplocation,
+            currentlocation: item.currentlocation,
+            image: item.image,
+          });
+          router.push("/individualResult");
+        }}
+      >
+        <HeadingOne>{item.name}</HeadingOne> <p> </p>
+        <p>{item.description}</p>
+        <p className="drop-location">
+          The {item.name} was found at {item.droplocation}
+        </p>
+        <img src={item.image}></img>
         {/* <p>Time: {item.timestamp.seconds}</p> */}
-        <button
-          onClick={() => {
-            currentItem.addItem({
-              name: item.name,
-              description: item.description,
-              droplocation: item.droplocation,
-              currentlocation: item.currentlocation,
-              image: item.image,
-            });
-            router.push("/individualResult");
-          }}
-        >
-          Investigate
-        </button>
       </div>
     );
   });
-  console.log("CURRENT ITEM:", currentItem);
-
-  // const goToResult = (result) => {
-  //   actions.updateResultAction(result);
-  //   console.log("RESULT DATA:", result, "STATE:", currentState);
-  //   router.push("/individualResult");
-  // };
-
-  // const addToMyBooks = (item) => ({
-  //   name: item.name,
-  //   description: item.description,
-  //   currentlocation: item.currentlocation,
-  //   image: item.image,
-  // });
-
-  // adddNew.addToMyBooks({
-  //   name: item.name,
-  //   description: item.description,
-  //   currentlocation: item.currentlocation,
-  //   image: item.image,
-  // });
 
   return (
     <>
@@ -111,7 +82,7 @@ export default function Lost() {
         <input type="submit" onClick={firebaseQuery}></input>
       </form>
 
-      <PageTitle>Results:</PageTitle>
+      {/* <PageTitle>Results:</PageTitle> */}
       <Grid>{foundItemslist}</Grid>
     </>
   );
