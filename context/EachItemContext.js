c2628350f7d7ad0775ab0a7c8336c8c38eccff64
Emil Schultz <emilrschultz@gmail.com>
2021-05-18 21:25:47 +0200

    feat: Array instead of object

--- context/EachItemContext.js
+++ context/EachItemContext.js
@@ -1,15 +1,15 @@
 import React, { createContext, useContext, useState } from "react";
 
 const EachItemContext = createContext({
-  currentItem: {},
+  currentItem: [],
   addItem: () => {},
 });
 
 export const EachItem = ({ children }) => {
-  const [currentItem, setCurrentItem] = useState();
+  const [currentItem, setCurrentItem] = useState([]);
 
   const addItem = (item) => {
-    setCurrentItem(item);
+    setCurrentItem([item]);
   };
 
   return (
