b8667584eb3da1574ab31005dab9d9b940d33be3
Emil Schultz <emilrschultz@gmail.com>
2021-05-19 22:41:58 +0200

    style: viewport instead of procent

--- themeConfig/index.jsx
+++ themeConfig/index.jsx
@@ -23,8 +23,8 @@ export const GlobalStyles = createGlobalStyle`
 }
 
   html, body {
-    height: 100%;
-    width: 100%;
+    height: 100vh;
+    width: 100vw;
     margin: 0;
     padding: 0;
     scroll-behavior: smooth;
